045ac917f148225afed1de691e74ec9097fb57e7
ZULQAR NAIN <zulqarnainhumbly258@gmail.com>
2024-12-27 20:11:55 +0500

    deployed on the aws with cicd pipeline

--- application.py
+++ application.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
-
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
 
 
 application = FastAPI()
+application.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+templates_path = Path(__file__).parent / "templates"
+templates = Jinja2Templates(directory=str(templates_path))
+
 
 
 @application.get("/")
