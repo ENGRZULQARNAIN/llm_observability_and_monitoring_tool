9847ab53c3f9affc0accbc598e7f2e21a120bc14
ENGRZULQARNAIN <zulqarnainhumbly258@gmail.com>
2025-05-09 10:31:37 +0500

    db changes

--- application.py
+++ application.py
@@ -70,7 +70,7 @@ application.add_middleware(
 
 @application.get("/")
 async def read_items():
-    return {"message":"OBAM AI: v0.2.2"}
+    return {"message":"OBAM AI: v0.2.3"}
 
 
 application.include_router(auth_routers.router, prefix="/api/v1")
